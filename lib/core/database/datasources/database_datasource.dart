748d42a52b6a04a0888b71325f2dbae608c22581
sudo-melanin <amosemma93@gmail.com>
2026-09-22 16:52:30 +0100

    feat: translate database errors

--- lib/core/database/datasources/database_datasource.dart
+++ lib/core/database/datasources/database_datasource.dart
@@ -1,14 +1,37 @@
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:fpdart/fpdart.dart';
+
+import '../../error/error.dart';
 import '../app_database.dart';
 
 abstract interface class DatabaseDataSource {
-  Future<T> transaction<T>(Future<T> Function() action);
+  Future<Either<Failure, T>> transaction<T>(
+    Future<T> Function() action,
+  );
 }
 
 class DriftDatabaseDataSource implements DatabaseDataSource {
   const DriftDatabaseDataSource(this._database);
+
   final AppDatabase _database;
+
   @override
-  Future<T> transaction<T>(Future<T> Function() action) {
-    return _database.transaction(action);
+  Future<Either<Failure, T>> transaction<T>(
+    Future<T> Function() action,
+  ) async {
+    try {
+      final result = await _database.transaction(action);
+
+      return Right(result);
+    } on DriftWrappedException catch (error) {
+      return Left(DatabaseFailure(error.message));
+    } on InvalidDataException catch (error) {
+      return Left(DatabaseFailure(error.message));
+    } on SqliteException catch (error) {
+      return Left(DatabaseFailure(error.message));
+    } catch (error) {
+      return Left(UnexpectedFailure(error.toString()));
+    }
   }
 }
